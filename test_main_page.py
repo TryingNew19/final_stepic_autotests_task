d818a14154b3bbc1715d23f2d4372eaea95bc672
Kseniia <klimentova.k@ati.su>
2021-06-27 13:02:41 +0300

    go from page to page, second variant - inintialize login page in test file

--- test_main_page.py
+++ test_main_page.py
@@ -21,11 +21,20 @@ def test_guest_should_go_to_login_page(browser):
     page.open()  # открываем страницу
     page.should_be_login_url()
 
+# variant1
+# def test_guest_can_go_to_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com"
+#     page = MainPage(browser, link)
+#     page.open()
+#     login_page = page.go_to_login_page()
+#     login_page.should_be_login_page()
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
-    login_page = page.go_to_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
 def test_guest_should_see_login_form(browser):
