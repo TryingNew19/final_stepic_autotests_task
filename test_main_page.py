5dce39d1cf4e30a601203cadca3387d20d23c8fe
Kseniia <klimentova.k@ati.su>
2021-06-27 12:54:21 +0300

    go from page to page, first variant

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,14 @@
 from .Pages.main_page import MainPage
 from .Pages.login_page import LoginPage
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+# def test_guest_can_go_to_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+#     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+#     page.open()                      # открываем страницу
+#     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
-#запуск pytest -v --tb=line --language=en test_main_page.py
+# запуск pytest -v --tb=line --language=en test_main_page.py
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -20,6 +20,13 @@ def test_guest_should_go_to_login_page(browser):
     page = LoginPage(browser, link)
     page.open()  # открываем страницу
     page.should_be_login_url()
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
