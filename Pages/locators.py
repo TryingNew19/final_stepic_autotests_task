d8eee76b92fa551793467e74fa05d08f6f161136
Kseniia <klimentova.k@ati.su>
2021-07-08 10:38:47 +0300

    check going to login page from product page

--- Pages/locators.py
+++ Pages/locators.py
@@ -5,7 +5,6 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
@@ -17,3 +16,7 @@ class ProductPageLocators():
     BOOK_PRICE = (By.CSS_SELECTOR, "p.price_color")
     BASKET_PRICE = (By.CSS_SELECTOR, ".alertinner p strong")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-safe:nth-child(1) .alertinner")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from .Pages.product_page import ProductPage
 import pytest
+from .Pages.login_page import LoginPage
 
 
 # @pytest.mark.parametrize('param_link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -42,6 +43,21 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()  # открываем страницу
     page.add_to_basket()  # добавляем товар в корзину
     page.should_disappear_message() #Проверяем, что нет сообщения об успехе с помощью is_disappeared
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
 
 
     #запуск pytest - s test_product_page.py
