1db0a51f8a081c7a37f1273c072dce32fb69a99c
Kseniia <klimentova.k@ati.su>
2021-07-09 20:32:20 +0300

    setup fixture added and register user

--- Pages/login_page.py
+++ Pages/login_page.py
@@ -1,6 +1,5 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
-import time
 
 
 class LoginPage(BasePage):
@@ -22,15 +21,6 @@ class LoginPage(BasePage):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
 
     def register_new_user(self, email, password):
-        # assert self.is_element_present(*LoginPageLocators.REGISTER_EMAIL), "No email field"
-        # email_field = self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL)
-        # email_field.send_keys(email)
-        # assert self.is_element_present(*LoginPageLocators.REGISTER_PASSWORD1), "No password1 field"
-        # password1_field = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD1)
-        # password1_field.send_keys(password)
-        # assert self.is_element_present(*LoginPageLocators.REGISTER_PASSWORD2), "No password2 field"
-        # password2_field = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD2)
-        # password2_field.send_keys(password)
         self.send_keys_if_element_present(*LoginPageLocators.REGISTER_EMAIL, email, "email")
         self.send_keys_if_element_present(*LoginPageLocators.REGISTER_PASSWORD1, password, "password1")
         self.send_keys_if_element_present(*LoginPageLocators.REGISTER_PASSWORD2, password, "password2")
